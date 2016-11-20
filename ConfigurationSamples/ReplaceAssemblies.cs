5b034fdc47a3b03426e52f72970838269d9c319b
trailmax <trailmax1@gmail.com>
2016-11-20 01:27:05 +0000

    Sagas Project; configuration samples

---
+++ ConfigurationSamples/ReplaceAssemblies.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using NSaga;
+
+namespace ConfigurationSamples
+{
+    class ReplaceAssemblies
+    {
+        public ReplaceAssemblies()
+        {
+            var builder = Wireup.UseInternalContainer()
+                                .ReplaceAssembliesToScan(new Assembly[] { typeof(ReplaceAssemblies).Assembly })
+                                .AddAssembliesToScan(new Assembly[] {typeof(VerySimpleSaga).Assembly});
+        }
+    }
+}
