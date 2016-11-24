ae50e67183d0ea92acc5b28cd41a52cd7c057bb9
trailmax <trailmax1@gmail.com>
2016-11-24 14:59:16 +0000

    Updated NSaga to latest; Added Diagnostics sample

---
+++ Diagnostics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NSaga;
+
+namespace Diagnostics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var validator = new NSagaConfigurationValidator(new[] { typeof(VerySimpleSaga).Assembly });
+
+            validator.AssertConfigurationIsValid(); // exceptions should be thrown in case of errors
+
+            Console.WriteLine("Validation is complete");
+            Console.ReadKey();
+        }
+    }
+}
