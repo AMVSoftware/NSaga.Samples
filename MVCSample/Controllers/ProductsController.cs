b50e48ff4fadcaff5c21bc99581192ec52c6fb96
trailmax <trailmax1@gmail.com>
2017-02-02 16:50:14 +0000

    changed branding on layout

--- MVCSample/Controllers/ProductsController.cs
+++ MVCSample/Controllers/ProductsController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using MVCSample.Models;
+
+
 
 namespace MVCSample.Controllers
 {
